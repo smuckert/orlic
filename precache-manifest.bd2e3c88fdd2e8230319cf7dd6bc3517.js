14c2de9aac76156ac7da777ea4faba0866de6a53
smuckert <magnus@houseofradon.com>
2020-01-08 10:36:22 +0100

    Updates

---
+++ precache-manifest.bd2e3c88fdd2e8230319cf7dd6bc3517.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff07c16c07cb0401209e7ca41e52e61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e204e7fd5c9e5121e882",
+    "url": "/static/css/main.b0904127.chunk.css"
+  },
+  {
+    "revision": "6ff32c1e266d3141c455",
+    "url": "/static/js/2.650f2459.chunk.js"
+  },
+  {
+    "revision": "be8d5874258829862d2955f3a820b124",
+    "url": "/static/js/2.650f2459.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e204e7fd5c9e5121e882",
+    "url": "/static/js/main.2d64e5d3.chunk.js"
+  },
+  {
+    "revision": "a34933d7015af824a514",
+    "url": "/static/js/runtime-main.e9101d03.js"
+  },
+  {
+    "revision": "3ee814b66f52a6c245310c46c49af785",
+    "url": "/static/media/font-bold.3ee814b6.woff"
+  },
+  {
+    "revision": "2757086098f95b44d0e69ce0f3f5222c",
+    "url": "/static/media/font-regular.27570860.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/orlic/precache-manifest.f855ab4650a25ee87bedd3c08180c563.js"
+  "/precache-manifest.bd2e3c88fdd2e8230319cf7dd6bc3517.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/orlic/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
